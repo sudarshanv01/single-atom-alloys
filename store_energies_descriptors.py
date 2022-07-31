1efa1ddfd8962b2edf3267d82fc1fc8b200b886e
Sudarshan Vijay <svijay@lbl.gov>
2022-07-31 15:15:21 -0700

    Change the parsing to allow for elementals as well

--- store_energies_descriptors.py
+++ store_energies_descriptors.py
@@ -23,71 +23,69 @@ if __name__ == '__main__':
     # Path to the vasprun folder
     vaspruns_path = "vaspruns"
 
-    intermetallics = loadfn("intermetallics.json")
-    elementals = loadfn("elementals.json")
+    for type_calc in ['elementals']:
+        # Data is stored in different files
+        dict_data = loadfn(f"{type_calc}.json")
+        # Store moments by _id
+        moments = defaultdict(dict)
+        ads_energy_dict = defaultdict(dict)
+        for mp_id in dict_data:
+            # Get the slab id
+            _id = dict_data[mp_id]["slabs"][0]["id"]
 
-    # Store moments by _id
-    moments = defaultdict(dict)
-    ads_energy_dict = defaultdict(dict)
+            # Make a separate plot for each intermetallic
+            fig, ax = plt.subplots(1, 1, figsize=(4,6), constrained_layout=True)
+            ax.set_ylabel('Energy (eV)')
+            ax.set_xlabel('Projected DOS')
 
-    for mp_id in intermetallics:
+            # Get the adsorption site
+            ads_site = dict_data[mp_id]["adsorption"][0]["ads_site"]
+            # Get the adsorption energy
+            ads_energy = dict_data[mp_id]["adsorption"][0]["ads_energy"]
+            ads_energy_dict[_id] = ads_energy
 
-        # Get the slab id
-        _id =intermetallics[mp_id]["slabs"][0]["id"]
+            # Get the vasprun file
+            vr = Vasprun(os.path.join(vaspruns_path, type_calc, "slab", _id + "_vasprun.xml.gz"))
 
-        # Make a separate plot for each intermetallic
-        fig, ax = plt.subplots(1, 1, figsize=(4,6), constrained_layout=True)
-        ax.set_ylabel('Energy (eV)')
-        ax.set_xlabel('Projected DOS')
+            # Store the dos
+            dos = vr.complete_dos
+            structure = dos.structure 
 
-        # Get the adsorption site
-        ads_site = intermetallics[mp_id]["adsorption"][0]["ads_site"]
-        # Get the adsorption energy
-        ads_energy = intermetallics[mp_id]["adsorption"][0]["ads_energy"]
-        ads_energy_dict[_id] = ads_energy
+            # Get the metal atom to which the CO is bound
+            metal_solute = structure[ads_site].specie.symbol
 
-        # Get the vasprun file
-        vr = Vasprun(os.path.join(vaspruns_path, "intermetallics", "slab", _id + "_vasprun.xml.gz"))
+            # Extract the projected density of states
+            pdos = dos.get_site_spd_dos(structure[ads_site])[OrbitalType.d]
 
-        # Store the dos
-        dos = vr.complete_dos
-        structure = dos.structure 
+            # Make x and y variables to plot
+            pdos_extract = pdos.get_densities()
+            energy = pdos.energies - pdos.efermi
 
-        # Get the metal atom to which the CO is bound
-        metal_solute = structure[ads_site].specie.symbol
+            # Get the band centres and widths
+            center_ase, width_ase = get_distribution_moment(energy, pdos_extract,(1,2))
+            popt, pcov = curve_fit(semi_ellipse, energy, pdos_extract, p0=[center_ase, width_ase, 1])
+            centre, width, amp = popt
 
-        # Extract the projected density of states
-        pdos = dos.get_site_spd_dos(structure[ads_site])[OrbitalType.d]
+            # Plot the band centre and width as 
+            ax.plot(pdos_extract, energy, '-', color='k')
+            ax.fill_between(pdos_extract, 0, energy, color='k', alpha=0.2)
+            ax.axhline(center_ase, color='tab:red', linestyle='--')
+            ax.axhline(center_ase + width_ase, color='tab:blue', linestyle='--')
+            ax.axhline(center_ase - width_ase, color='tab:blue', linestyle='--')
 
-        # Make x and y variables to plot
-        pdos_extract = pdos.get_densities()
-        energy = pdos.energies - pdos.efermi
+            moments[_id]["d_band_centre"] = centre
+            moments[_id]["d_band_width"] = width
+            moments[_id]["metal_solute"] = metal_solute
 
-        # Get the band centres and widths
-        center_ase, width_ase = get_distribution_moment(energy, pdos_extract,(1,2))
-        popt, pcov = curve_fit(semi_ellipse, energy, pdos_extract, p0=[center_ase, width_ase, 1])
-        centre, width, amp = popt
+            # Plot semi-ellipse
+            ax.plot(semi_ellipse(energy, centre, width, amp), energy, '-', color='tab:green')
 
-        # Plot the band centre and width as 
-        ax.plot(pdos_extract, energy, '-', color='k')
-        ax.fill_between(pdos_extract, 0, energy, color='k', alpha=0.2)
-        ax.axhline(center_ase, color='tab:red', linestyle='--')
-        ax.axhline(center_ase + width_ase, color='tab:blue', linestyle='--')
-        ax.axhline(center_ase - width_ase, color='tab:blue', linestyle='--')
-
-        moments[_id]["d_band_centre"] = centre
-        moments[_id]["d_band_width"] = width
-        moments[_id]["metal_solute"] = metal_solute
-
-        # Plot semi-ellipse
-        ax.plot(semi_ellipse(energy, centre, width, amp), energy, '-', color='tab:green')
-
-        ax.set_ylim([center_ase-7, center_ase+7])
-        # Save the figure
-        fig.savefig(os.path.join("plots", "intermetallics", _id + ".png"))
+            ax.set_ylim([center_ase-7, center_ase+7])
+            # Save the figure
+            fig.savefig(os.path.join("plots", "intermetallics", _id + ".png"))
     
-    with open(f"outputs/pdos_moments.json", 'w') as handle:
-        json.dump(moments, handle, indent=4)
-    # Store adsorption energies
-    with open(f"outputs/ads_energy.json", 'w') as handle:
-        json.dump(ads_energy_dict, handle, indent=4)
+        with open(f"outputs/{type_calc}_pdos_moments.json", 'w') as handle:
+            json.dump(moments, handle, indent=4)
+        # Store adsorption energies
+        with open(f"outputs/{type_calc}_ads_energy.json", 'w') as handle:
+            json.dump(ads_energy_dict, handle, indent=4)
