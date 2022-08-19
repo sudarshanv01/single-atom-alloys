f5b98f0bf78b2593c4a53d38d405391c9325ec17
Sudarshan Vijay <vijays@fysik.dtu.dk>
2022-08-18 19:44:13 -0700

    Changes to the fitting routines

--- plot_colormap.py
+++ plot_colormap.py
@@ -1,20 +1,31 @@
 import pickle
 import json
+from collections import defaultdict
+
 import numpy as np
 import matplotlib.pyplot as plt
-from catchemi import FitParametersNewnsAnderson
 from monty.serialization import loadfn
+
+from catchemi import FitParametersNewnsAnderson
+from create_coupling_elements import create_coupling_elements
 from plot_params import get_plot_params
 get_plot_params()
 
+FIRST_ROW   = [ 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn']
+SECOND_ROW  = [ 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd']
+THIRD_ROW   = [ 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl'] 
+
 if __name__ == '__main__':
+    """Create a series of plots based on varying epsilon_d and 
+    w_d freely. The only difference from previous plots is that the
+    Vak is proportional to epsilon_d based on the relation in the
+    JCP paper and w_d has no influence on the Vak."""
 
-    # Create a contour plot of the energy matrix
-    fig, ax = plt.subplots(1, 3, figsize=(9, 3), constrained_layout=True)
-
-    data_from_dos_calculation = json.load(open(f"outputs/intermetallics_pdos_moments.json")) 
-
-    # Read in the adsorbate parameters
+    # ==== Adsorbate parameters ====
+    # Each adsorbate has different alpha, beta, delta0
+    # and epsilon_a parameters. epsilson_sp min, max
+    # are the energy levels corresponding to the 
+    # minimum and maximum of the sp-augmentation.
     ads_parameters = loadfn("CO_parameters.json")
     alpha = ads_parameters['alpha']
     beta = ads_parameters['beta']
@@ -23,87 +34,177 @@ if __name__ == '__main__':
     constant_offset = ads_parameters['constant_offset'] 
     EPS_SP_MIN = -15
     EPS_SP_MAX = 15
-
-    # Spline parameters
-    with open(f"outputs/spline_objects.pkl", 'rb') as f:
-        spline_objects = pickle.load(f)
-
-    # Parameters to change delta
-    widths = np.linspace(0.2, 12, 15)
-    eps_ds = np.linspace(-6, 5.5, 15)
+    # Running values of the energy
     EPS_VALUES = np.linspace(-30, 30, 1000,) 
 
-    # Store the chemisorption energy
-    energy_matrix_hyb = np.zeros((len(widths), len(eps_ds)))
-    energy_matrix_ortho = np.zeros((len(widths), len(eps_ds)))
-    energy_matrix_tot = np.zeros((len(widths), len(eps_ds)))
+    # ==== Transition Metal parameters ====
+    # Spline parameters for the transition metals. Currently
+    # stored as a pickle file, directly from the JCP paper.
+    with open(f"outputs/spline_objects.pkl", 'rb') as f:
+        spline_objects = pickle.load(f)
+    # Data from Andrew's calculations.
+    data_from_dos_calculation = json.load(open(f"outputs/intermetallics_pdos_moments.json")) 
+    # Data from the LMTO calculation (for the transition metals).
+    data_from_LMTO = json.load(open('inputs/data_from_LMTO.json'))
+    # Each row has a different range of epsilon_d and w_d.
+    minmax_parameters = json.load(open('outputs/minmax_parameters.json'))
+    # s is a bulk parameter.
+    s_data = data_from_LMTO['s']
+    # Also need the Anderson band width (bulk parameter).
+    anderson_band_width_data = data_from_LMTO['anderson_band_width']
+    
+    # ==== Plot parameters for the 2D plot ====
+    GRID_SIZE = 7
+    # Create a different subplot for each row and a
+    # a different plot for each type of energy. This 
+    # will be an imshow 2D plot.
+    figsize = (10,3)
+    figh, axh = plt.subplots(1, 3, figsize=figsize, constrained_layout=True)
+    figo, axo = plt.subplots(1, 3, figsize=figsize, constrained_layout=True)
+    figc, axc = plt.subplots(1, 3, figsize=figsize, constrained_layout=True)
+    # Width is going to be a free parameter in this
+    # model. We do not know if it is or isn't, but we do know
+    # that each contour along its variation corresponds to a
+    # type of metal (intermetallics, transition metals, or others...)
+    width_values = np.linspace(0.3, 6, GRID_SIZE)
 
-    for i, width in enumerate(widths):
-        widths_const = np.ones(len(eps_ds)) * width
+    # Iterate over the rows of the transition metals that lead
+    # each row has different epsilon_d, w_d and other parameters.
+    # This variation allows us to make separate figures for the 
+    # different rows, for chemisorption, ortho and hybridisation.
+    for j, metal_row in enumerate([FIRST_ROW, SECOND_ROW, THIRD_ROW]):
 
-        kwargs_fit = dict(
-            eps_sp_min = EPS_SP_MIN,
-            eps_sp_max = EPS_SP_MAX,
-            eps = EPS_VALUES,
-            Delta0_mag = delta0,
-            Vsd = np.ones(len(eps_ds)).tolist(), 
-            width = widths_const.tolist(), 
-            eps_a = eps_a,
-            verbose = True,
-            store_hyb_energies = True,
-        )
+        # Store the parameters for the different rows.
+        parameters_metal = defaultdict(list)
 
-        fitting_function =  FitParametersNewnsAnderson(**kwargs_fit)
+        # get the metal fitting parameters
+        s_fit = spline_objects[j]['s']
+        Delta_anderson_fit = spline_objects[j]['Delta_anderson']
+        wd_fit = spline_objects[j]['width']
+        eps_d_fit = spline_objects[j]['eps_d']
 
-        # Get the final hybridisation energy
-        final_params = alpha + beta + constant_offset
-        chemi_energy = fitting_function.fit_parameters(final_params, eps_ds) 
-        optimised_hyb = fitting_function.hyb_energy
-        optimised_ortho = fitting_function.ortho_energy
+        filling_min, filling_max = minmax_parameters[str(j)]['filling']
+        filling_range = np.linspace(filling_max, filling_min, GRID_SIZE)
+        eps_d_min, eps_d_max = minmax_parameters[str(j)]['eps_d']
+        eps_d_range = np.linspace(eps_d_min, eps_d_max, GRID_SIZE)
 
-        # Store the energy matrix
-        energy_matrix_hyb[i, :] = optimised_hyb 
-        energy_matrix_ortho[i,:] = optimised_ortho
-        energy_matrix_tot[i,:] = chemi_energy
+        # Store the energies for each row.
+        energy_matrix_hyb = np.zeros((GRID_SIZE, GRID_SIZE))
+        energy_matrix_ortho = np.zeros((GRID_SIZE, GRID_SIZE))
+        energy_matrix_tot = np.zeros((GRID_SIZE, GRID_SIZE))
 
-    # Plot the contour
-    energy_matrix_hyb = energy_matrix_hyb.T
-    energy_matrix_ortho = energy_matrix_ortho.T
-    energy_matrix_tot = energy_matrix_tot.T
+        # Iterate over the filling range to get the required
+        # epsilon_d values, each epsilon_d value corresponds to 
+        # a transition metal w_d, which is going to be the 
+        # contour line. The single atom alloys are most likely
+        # not going to be on this contour line.
+        for k, filling in enumerate(filling_range):
+            # Continuous setting of parameters for each 
+            # continous variation of the metal
+            width = wd_fit(filling) 
+            eps_d = eps_d_fit(filling) 
+            Vsdsq = create_coupling_elements(s_metal=s_fit(filling),
+                                            s_Cu=s_data['Cu'],
+                                            anderson_band_width=Delta_anderson_fit(filling),
+                                            anderson_band_width_Cu=anderson_band_width_data['Cu'],
+                                            r=s_fit(filling),
+                                            r_Cu=s_data['Cu'],
+                                            normalise_by_Cu=True,
+                                            normalise_bond_length=True
+                                            )
+            Vsd = np.sqrt(Vsdsq)
+            parameters_metal['Vsd'].append(Vsd)
+            parameters_metal['eps_d'].append(eps_d)
+            parameters_metal['width'].append(width)
+            parameters_metal['filling'].append(filling)
+            parameters_metal['no_of_bonds'] = np.ones(GRID_SIZE)
+        
+        # We will iterate over the different epsilon_d values
+        # varying around the different width values to generate
+        # a 2D plot. 
+        # epsilon_d is determined by the filling 
+        for indexw, width in enumerate(width_values): 
+            # width is a free parameter
+            widths_fixed = np.ones(GRID_SIZE) * width
+            widths_fixed = widths_fixed.tolist()
+            kwargs_fit = dict(
+                eps_sp_min = EPS_SP_MIN,
+                eps_sp_max = EPS_SP_MAX,
+                eps = EPS_VALUES,
+                Delta0_mag = delta0,
+                Vsd = parameters_metal['Vsd'],
+                width = widths_fixed, 
+                eps_a = eps_a,
+                verbose = True,
+                store_hyb_energies = True,
+                # precision = 50,
+            )
+            fitting_function =  FitParametersNewnsAnderson(**kwargs_fit)
+                
+            # Get the energies
+            final_params = alpha + beta + constant_offset
+            chemi_energy = fitting_function.fit_parameters(final_params, parameters_metal['eps_d']) 
+            optimised_hyb = fitting_function.hyb_energy
+            optimised_ortho = fitting_function.ortho_energy
 
-    cax = ax[0].contourf(widths, eps_ds, energy_matrix_hyb, levels=100)
-    cbar = fig.colorbar(cax, ax=ax[0])
-    ax[0].contour(widths, eps_ds, energy_matrix_hyb, levels=5, colors='tab:gray', linewidths=0.5)
-    # Plot the ortho energies
-    cax = ax[1].contourf(widths, eps_ds, energy_matrix_ortho, levels=100)
-    cbar = fig.colorbar(cax, ax=ax[1])
-    ax[1].contour(widths, eps_ds, energy_matrix_ortho, levels=5, colors='tab:gray', linewidths=0.5)
-    # Plot the total energies
-    cax = ax[2].contourf(widths, eps_ds, energy_matrix_tot, levels=100)
-    cbar = fig.colorbar(cax, ax=ax[2])
-    ax[2].contour(widths, eps_ds, energy_matrix_tot, levels=5, colors='tab:gray', linewidths=0.5)
+            # Store the energy matrix
+            energy_matrix_hyb[indexw, :] = optimised_hyb 
+            energy_matrix_ortho[indexw,:] = optimised_ortho
+            energy_matrix_tot[indexw,:] = chemi_energy
 
+        # Plot the contour
+        energy_matrix_hyb = energy_matrix_hyb.T
+        energy_matrix_ortho = energy_matrix_ortho.T
+        energy_matrix_tot = energy_matrix_tot.T
+
+        # Plot the contour lines for the different rows.
+        cax = axh[j].contourf(width_values, parameters_metal['eps_d'], energy_matrix_hyb, levels=100)
+        cbar = figh.colorbar(cax, ax=axh[j])
+        axh[j].contour(width_values, parameters_metal['eps_d'], energy_matrix_hyb, levels=5, colors='tab:gray', linewidths=0.5)
+
+        # Plot the ortho energies
+        cax = axo[j].contourf(width_values, parameters_metal['eps_d'], energy_matrix_ortho, levels=100)
+        cbar = figo.colorbar(cax, ax=axo[j])
+        axo[j].contour(width_values, parameters_metal['eps_d'], energy_matrix_ortho, levels=5, colors='tab:gray', linewidths=0.5)
+
+        # Plot the total energies
+        cax = axc[j].contourf(width_values, parameters_metal['eps_d'], energy_matrix_tot, levels=100)
+        cbar = figc.colorbar(cax, ax=axc[j])
+        axc[j].contour(width_values, parameters_metal['eps_d'], energy_matrix_tot, levels=5, colors='tab:gray', linewidths=0.5)
+
+        # Plot the epsilon_d and width values
+        axh[j].plot(parameters_metal['width'], parameters_metal['eps_d'], 'o', color='k', markersize=2)
+        axo[j].plot(parameters_metal['width'], parameters_metal['eps_d'], 'o', color='k', markersize=2)
+        axc[j].plot(parameters_metal['width'], parameters_metal['eps_d'], 'o', color='k', markersize=2)
+
+    # Plot the points from Andrew's calculation
     for _id in data_from_dos_calculation:
-
         # get the parameters from DFT calculations
         metal = data_from_dos_calculation[_id]['metal_solute']
         width = data_from_dos_calculation[_id]['d_band_width']
         d_band_centre = data_from_dos_calculation[_id]['d_band_centre']
+        for ax in [axc, axo, axh]:
+            # Choose the index of the plot based on the row number
+            if metal in FIRST_ROW:
+                index = 0
+            elif metal in SECOND_ROW:
+                index = 1
+            elif metal in THIRD_ROW:
+                index = 2
+            ax[index].plot(width, d_band_centre, '*', color='k', markersize=2)
+
+    # Label the axes
+    for ax in [axc, axo, axh]:
         for a in ax:
-            a.plot(width, d_band_centre, 'o', color='k', markersize=2)
+            a.set_xlabel('Width (eV)')
+            a.set_ylabel('$\epsilon_d$')
 
-    for a in ax:
-        a.set_xlabel(r'width (eV)')
-        a.set_ylabel(r'$\epsilon_d$ (eV)')
-        a.set_xlim(0.2, 12)
-
-    ax[0].set_title(r'Hybridisation')
-    ax[1].set_title(r'Orthogonalisation')
-    ax[2].set_title(r'Chemisorption')
+            # Set the x-axis limits and the y-axis limits
+            # for the different plots
+             
 
 
-
-    fig.savefig('outputs/figure_colormap.png', dpi=300)
-
-
-
+    # Save the figure
+    figc.savefig('outputs/chemi_energy.png', dpi=300)
+    figo.savefig('outputs/ortho_energy.png', dpi=300)
+    figh.savefig('outputs/hyb_energy.png', dpi=300)
--- store_energies_descriptors.py
+++ store_energies_descriptors.py
@@ -1,16 +1,25 @@
 """Store the energies and band-centres."""
+import logging
 import os
+import json
+
+from collections import defaultdict
+
 import matplotlib.pyplot as plt
 import numpy as np
-import json
-from collections import defaultdict
+
 from scipy.optimize import curve_fit
 from monty.serialization import loadfn
+
 from pymatgen.electronic_structure.core import OrbitalType
 from pymatgen.io.vasp import Vasprun
+
 from ase.dft import get_distribution_moment
+from ase import units
 
 def semi_ellipse(energies, eps_d, width, amp):
+    """Functional form of a semi-ellipse to allow
+    for fitting the density of states."""
     energy_ref = ( energies - eps_d ) / width
     delta = np.zeros(len(energies))
     for i, eps_ in enumerate(energy_ref):
@@ -18,18 +27,56 @@ def semi_ellipse(energies, eps_d, width, amp):
             delta[i] = amp * (1 - eps_**2)**0.5
     return delta
 
+
+def create_coupling_elements(s_metal, s_Cu, 
+    anderson_band_width, anderson_band_width_Cu, 
+    r=None, r_Cu=None, normalise_bond_length=False,
+    normalise_by_Cu=True):
+    """Create the coupling elements based on the Vsd
+    and r values. The Vsd values are identical to those
+    used in Ruban et al. The assume that the bond lengths
+    between the metal and adsorbate are the same. Everything
+    is referenced to Cu, as in the paper by Ruban et al."""
+    Vsdsq = s_metal**5 * anderson_band_width
+    Vsdsq_Cu = s_Cu**5 * anderson_band_width_Cu 
+    if normalise_by_Cu:
+        Vsdsq /= Vsdsq_Cu
+    if normalise_bond_length:
+        assert r is not None
+        if normalise_by_Cu: 
+            assert r_Cu is not None
+            Vsdsq *= r_Cu**8 / r**8
+        else:
+            Vsdsq /= r**8
+    return Vsdsq
+
+
 if __name__ == '__main__':
     """Store the energies and the d-band centres in a json file."""
+
+    logging.basicConfig(level=logging.INFO)
+
     # Path to the vasprun folder
     vaspruns_path = "vaspruns"
+    logging.info(f"Reading vaspruns from {vaspruns_path}")
 
-    for type_calc in ['elementals']:
+    # Input parameters
+    data_from_LMTO = json.load(open('inputs/data_from_LMTO.json'))
+    logging.info(f"Reading LMTO input.")
+
+    # Parse data from the LMTO calculation
+    s_data = data_from_LMTO['s']
+    anderson_band_width_data = data_from_LMTO['anderson_band_width']
+
+    for type_calc in ['intermetallics', 'elementals']:
+
         # Data is stored in different files
         dict_data = loadfn(f"{type_calc}.json")
-        # Store moments by _id
-        moments = defaultdict(dict)
-        ads_energy_dict = defaultdict(dict)
+
+        output_data = defaultdict(dict)
+
         for mp_id in dict_data:
+
             # Get the slab id
             _id = dict_data[mp_id]["slabs"][0]["id"]
 
@@ -40,9 +87,11 @@ if __name__ == '__main__':
 
             # Get the adsorption site
             ads_site = dict_data[mp_id]["adsorption"][0]["ads_site"]
+
+
             # Get the adsorption energy
             ads_energy = dict_data[mp_id]["adsorption"][0]["ads_energy"]
-            ads_energy_dict[_id] = ads_energy
+            logging.info(f"Adsorption energy for {mp_id} is {ads_energy}")
 
             # Get the vasprun file
             vr = Vasprun(os.path.join(vaspruns_path, type_calc, "slab", _id + "_vasprun.xml.gz"))
@@ -50,6 +99,22 @@ if __name__ == '__main__':
             # Store the dos
             dos = vr.complete_dos
             structure = dos.structure 
+
+            # Get the bond length from the LMTO calculations
+            ads_metal = structure[ads_site].specie.symbol
+            bond_length = data_from_LMTO['s'][ads_metal]*units.Bohr
+            bond_length_Cu = data_from_LMTO['s']['Cu']*units.Bohr 
+            Vsdsq = create_coupling_elements(s_metal=s_data[ads_metal],
+                s_Cu=s_data['Cu'],
+                anderson_band_width=anderson_band_width_data[ads_metal],
+                anderson_band_width_Cu=anderson_band_width_data['Cu'],
+                r=bond_length,
+                r_Cu=bond_length_Cu,
+                normalise_bond_length=True,
+                normalise_by_Cu=True)
+            
+            Vsd = Vsdsq**0.5
+            logging.info(f"Coupling element for {ads_metal} is {Vsd}")
 
             # Get the metal atom to which the CO is bound
             metal_solute = structure[ads_site].specie.symbol
@@ -59,6 +124,11 @@ if __name__ == '__main__':
 
             # Make x and y variables to plot
             pdos_extract = pdos.get_densities()
+
+            # Normalise the extracted pdos
+            normalising_integral = np.trapz(pdos_extract, dos.energies)
+            pdos_extract /= normalising_integral
+
             energy = pdos.energies - pdos.efermi
 
             # Get the band centres and widths
@@ -73,9 +143,17 @@ if __name__ == '__main__':
             ax.axhline(center_ase + width_ase, color='tab:blue', linestyle='--')
             ax.axhline(center_ase - width_ase, color='tab:blue', linestyle='--')
 
-            moments[_id]["d_band_centre"] = centre
-            moments[_id]["d_band_width"] = width
-            moments[_id]["metal_solute"] = metal_solute
+            output_data[_id]["d_band_centre"] = centre
+            output_data[_id]["d_band_width"] = width
+            output_data[_id]["metal_solute"] = metal_solute
+            # Store the normalised density of states as well
+            output_data[_id]["pdos"] = pdos_extract.tolist()
+            # Store the energy as well
+            output_data[_id]["energy_grid"] = energy.tolist()
+            output_data[_id]["ads_energy"] = ads_energy
+            # Store the coupling element
+            output_data[_id]["Vsd"] = float(Vsd)
+            logging.info(f"Stored {_id}")
 
             # Plot semi-ellipse
             ax.plot(semi_ellipse(energy, centre, width, amp), energy, '-', color='tab:green')
@@ -83,9 +161,7 @@ if __name__ == '__main__':
             ax.set_ylim([center_ase-7, center_ase+7])
             # Save the figure
             fig.savefig(os.path.join("plots", "intermetallics", _id + ".png"))
+            plt.close(fig)
     
         with open(f"outputs/{type_calc}_pdos_moments.json", 'w') as handle:
-            json.dump(moments, handle, indent=4)
-        # Store adsorption energies
-        with open(f"outputs/{type_calc}_ads_energy.json", 'w') as handle:
-            json.dump(ads_energy_dict, handle, indent=4)
+            json.dump(output_data, handle, indent=4)
